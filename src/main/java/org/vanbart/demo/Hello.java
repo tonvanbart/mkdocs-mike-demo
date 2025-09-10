769efa67acc25da720cd7bfba9d0c68ee4d1b8a4
Ton van Bart <ton.vanbart@kpn.com>
2025-09-10 20:30:03 +0200

    Initial Java project

---
+++ src/main/java/org/vanbart/demo/Hello.java
@@ -0,0 +1,11 @@
+package org.vanbart.demo;
+
+/**
+ * The super awesome Hello app.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
